c777dd4e805be746773eb219fb67da6ba8dd5211
mtvk100968 <tirupathim@gmail.com>
2024-12-21 20:43:48 +0530

    temp after the wreckage

--- lib/components/forms/sell_land/steps/step1_basic_details.dart
+++ lib/components/forms/sell_land/steps/step1_basic_details.dart
@@ -1,23 +1,73 @@
 // lib/components/widgets/steps/step1_basic_details.dart
-
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import '../../../../providers/property_provider.dart';
 import '../../../../utils/validators.dart';
+import '../../../../utils/format.dart';
+import '../../../../models/user_model.dart';
+import '../../../../providers/property_provider.dart';
 
-class Step1BasicDetails extends StatelessWidget {
+class Step1BasicDetails extends StatefulWidget {
   final GlobalKey<FormState> formKey;
 
   const Step1BasicDetails({Key? key, required this.formKey}) : super(key: key);
 
   @override
+  _Step1BasicDetailsState createState() => _Step1BasicDetailsState();
+}
+
+class _Step1BasicDetailsState extends State<Step1BasicDetails> {
+  final TextEditingController _phoneController = TextEditingController();
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _ownerNameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+
+  AppUser? currentUser;
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchCurrentUser();
+  }
+
+  Future<void> _fetchCurrentUser() async {
+    final user = FirebaseAuth.instance.currentUser;
+
+    if (user != null) {
+      // Attempt to fetch phone number from Firebase Auth
+      _phoneController.text = user.phoneNumber ?? '';
+
+      try {
+        // Fetch additional details from Firestore
+        final doc = await FirebaseFirestore.instance
+            .collection('users')
+            .doc(user.uid)
+            .get();
+
+        if (doc.exists) {
+          setState(() {
+            currentUser = AppUser.fromDocument(doc.data()!);
+            _emailController.text = currentUser?.email ?? '';
+            _phoneController.text = currentUser?.phoneNumber ??
+                user.phoneNumber ??
+                ''; // Use Firestore as fallback
+            _nameController.text = currentUser?.name ?? '';
+          });
+        }
+      } catch (e) {
+        print("Error fetching Firestore user data: $e");
+      }
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
     final propertyProvider = Provider.of<PropertyProvider>(context);
-
-    return SingleChildScrollView(
-        child: Form(
-      key: formKey,
+    return Form(
+      key: widget.formKey,
       child: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
@@ -42,6 +92,9 @@ class Step1BasicDetails extends StatelessWidget {
               initialValue: propertyProvider.name,
               validator: Validators.requiredValidator,
               onChanged: (value) => propertyProvider.setName(value),
+              inputFormatters: [
+                capitalizeWordsInputFormatter(),
+              ],
             ),
             SizedBox(height: 20),
             TextFormField(
@@ -50,6 +103,9 @@ class Step1BasicDetails extends StatelessWidget {
               validator: Validators.requiredValidator,
               onChanged: (value) =>
                   propertyProvider.setPropertyOwnerName(value),
+              inputFormatters: [
+                capitalizeWordsInputFormatter(),
+              ],
             ),
             SizedBox(height: 20),
             DropdownButtonFormField<String>(
@@ -72,6 +128,6 @@ class Step1BasicDetails extends StatelessWidget {
           ],
         ),
       ),
-    ));
+    );
   }
 }
--- lib/utils/format.dart
+++ lib/utils/format.dart
@@ -1,12 +1,31 @@
-// lib/utils/format.dart
+import 'package:flutter/services.dart';
 
 extension StringExtension on String {
   String capitalize() {
-    if (this.isEmpty) return this;
+    if (isEmpty) return this;
     return this[0].toUpperCase() + substring(1);
   }
 }
 
+String capitalizeEachWord(String input) {
+  if (input.isEmpty) return input;
+  return input
+      .split(' ')
+      .map((word) => word.isEmpty ? word : word.capitalize())
+      .join(' ');
+}
+
+TextInputFormatter capitalizeWordsInputFormatter() {
+  return TextInputFormatter.withFunction((oldValue, newValue) {
+    String transformed = capitalizeEachWord(newValue.text);
+    return newValue.copyWith(
+      text: transformed,
+      selection: TextSelection.collapsed(offset: transformed.length),
+    );
+  });
+}
+
+// Your existing price formatting functions remain unchanged
 String formatPrice(double price) {
   String formatValue(double value) {
     // Format value and remove .0 if it exists
@@ -20,13 +39,16 @@ String formatPrice(double price) {
     return '₹${formatValue(price / 10000000)}C';
   } else if (price >= 100000) {
     return '₹${formatValue(price / 100000)}L';
-  }
-  // else if (price >= 1000) {
-  //   return '₹${formatValue(price / 1000)}K';
-  // }
-  else {
+  } else {
     return '₹${formatValue(price)}';
   }
+}
+
+String formatIndianPrice(double price) {
+  final formatter = RegExp(r'(\d+?)(?=(\d\d)+(\d)(?!\d))');
+  return price
+      .toString()
+      .replaceAllMapped(formatter, (match) => '${match[1]},');
 }
 
 String formatValue(double value) {
